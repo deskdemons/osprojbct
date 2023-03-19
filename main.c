dbc85c41843be285c48e5abd10f92adf542bbf9f
Saurav Adhikari <69170305+ErSauravAdhikari@users.noreply.github.com>
2023-03-19 16:38:50 +0545

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
 
+
+int main() {
+
+  printf("OS CASE STUDY! BUILDING A PACKAGE FOR APLINE LINUX DISTRO");
+  printf("\nBUILT BY: SAURAV, RAJEEV AND PRASIDDHA");
+  printf("\nTHIS PACKDGE DOES NOTHING REALLY");
+  printf("\n");
+	
+}
